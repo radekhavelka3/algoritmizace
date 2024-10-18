621b117247bf6605b89860cb69880d5d058ce63c
radekhavelka3 <24304@student.vitkovickastredni.cz>
2024-10-18 16:16:34 +0200

    Add files via upload

---
+++ Lichá čísla do zadaného rozsahu/funkcefor.py
@@ -0,0 +1,5 @@
+N=int(input("Vlož hodnotu:"))
+
+for i in range(1,N,2):
+  print(i)
+print("konec")
---
+++ Lichá čísla do zadaného rozsahu/funkcewhile.py
@@ -0,0 +1,7 @@
+N=int(input("Vlož hodnotu:"))
+i=1
+
+while N-1>i:
+    i += 2
+    print(i)
+print("konec")
